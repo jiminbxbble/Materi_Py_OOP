0d4bffdfdb9fb258ca8f390813d2c6caf2b85ce6
Rafiqah Nailaturrahmah <130327408+jiminbxbble@users.noreply.github.com>
2025-11-17 15:23:44 +0700

    Update Abstraction.py

--- Abstraction.py
+++ Abstraction.py
@@ -1,14 +1,16 @@
 from abc import ABC, abstractmethod
-#contoh kelas abstrak dalam OOP
+
+# contoh kelas abstrak dalam OOP
 class Hewan(ABC):
     @abstractmethod
     def bergerak(self):
         pass
 
-#kelas Burung yang mewarisi dari kelas Hewan
+# kelas Burung yang mewarisi dari kelas Hewan
 class Burung(Hewan):
     def bergerak(self):
         print("Burung terbang di udara.")
 
-Burung = Burung()
-print(Burung.bergerak())  # Output: Burung terbang di udara.
+# membuat objek burung
+burung = Burung()
+burung.bergerak()  # Output: Burung terbang di udara.
