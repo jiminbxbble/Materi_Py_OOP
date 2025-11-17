5a0d972e2c5a41ebb26a46a8c11cff30f52227d2
Rafiqah Nailaturrahmah <130327408+jiminbxbble@users.noreply.github.com>
2025-11-17 15:56:58 +0700

    Update Encapsulation.py

--- Encapsulation.py
+++ Encapsulation.py
@@ -1,11 +1,16 @@
 # Contoh enkapsulasi dalam Python
 class BankAccount:
-    def __init__(self, saldo): # konstruktor
-        self.__saldo = saldo  # private variable
+    def __init__(self, saldo):  # konstruktor
+        self.__saldo = saldo    # private variable
 
     def deposit(self, jumlah):
-        self.__saldo += jumlah
-        # Menambahkan jumlah ke saldo
+        self.__saldo += jumlah  # Menambahkan jumlah ke saldo
 
-    def lihat_saldo(self):
+    def lihat_saldo(self): # Getter untuk saldo
         print(f"Saldo Anda saat ini: {self.__saldo}")
+
+
+# Contoh Penggunaan
+akun = BankAccount(100000)  # membuat objek dengan saldo awal 100 ribu
+akun.deposit(50000)         # menambah saldo
+akun.lihat_saldo()          # menampilkan saldo
