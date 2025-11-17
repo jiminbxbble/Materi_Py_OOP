1a7fc9590bb7c475ce61d2e4e1eb8c8ba3dc2d44
Rafiqah Nailaturrahmah <130327408+jiminbxbble@users.noreply.github.com>
2025-11-17 15:56:34 +0700

    Update Inheritance.py

--- Inheritance.py
+++ Inheritance.py
@@ -1,8 +1,15 @@
-#contoh pewarisan dalam OOP
+# contoh pewarisan dalam OOP
 class Animal:
     def speak(self):
         print("Bersuara")
-    
-class Dog(Animal): # Kelas Dog mewarisi dari kelas Animal
+
+class Dog(Animal):  # Kelas Dog mewarisi dari kelas Animal
     def speak(self):
         print("Guk Guk!")
+
+# Contoh penggunaan
+hewan = Animal() # Membuat objek dari kelas Animal
+anjing = Dog() # Membuat objek dari kelas Dog
+
+hewan.speak()   # Output: Bersuara
+anjing.speak()  # Output: Guk Guk!
